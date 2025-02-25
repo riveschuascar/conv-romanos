beb03f65a9fbb9d203e853d896a2bc790cc08cde
unknown <huascar.rivero@ucb.edu.bo>
2025-02-24 18:28:58 -0800

    fix: correcion al mapa para incluir al 10 en romano

--- src/convertidor-romanos.js
+++ src/convertidor-romanos.js
@@ -1,6 +1,6 @@
 function enteroARomano(num) {
   const mapa_conversion = [
-    [9, "IX"], [5, "V"], [4, "IV"], [1, "I"]
+    [10, "X"], [9, "IX"], [5, "V"], [4, "IV"], [1, "I"]
   ];
 
   let respuesta = "";
