82e4e002a781176b3d6d9d70841f5d3a88c4b8bc
unknown <huascar.rivero@ucb.edu.bo>
2025-02-24 18:48:13 -0800

    feat: ahora convierte numeros de 1-100

--- src/convertidor-romanos.js
+++ src/convertidor-romanos.js
@@ -1,6 +1,7 @@
 function enteroARomano(num) {
   const mapa_conversion = [
-    [10, "X"], [9, "IX"], [5, "V"], [4, "IV"], [1, "I"]
+    [100, "C"], [90, "XC"], [50, "L"], [40, "XL"],
+		[10, "X"], [9, "IX"], [5, "V"], [4, "IV"], [1, "I"]
   ];
 
   let respuesta = "";
