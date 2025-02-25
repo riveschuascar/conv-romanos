b7fdcbddbfc550fc37edc3880e83a0ed3ddd8283
unknown <huascar.rivero@ucb.edu.bo>
2025-02-24 18:26:58 -0800

    feat: conversor simple de 1-10 a romano

---
+++ src/convertidor-romanos.js
@@ -0,0 +1,13 @@
+function enteroARomano(num) {
+  const mapa_conversion = [
+    [9, "IX"], [5, "V"], [4, "IV"], [1, "I"]
+  ];
+
+  let respuesta = "";
+  for ([numero, simbolo] of mapa_conversion) {
+    while (num > numero) {
+			respuesta += simbolo;
+			num -= numero;
+		}
+  }
+}
